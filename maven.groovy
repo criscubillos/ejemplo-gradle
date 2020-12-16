2929bce1e001c86bef84b53d744175728ee186ac
Cristian <cristian.cubillos@editrade.cl>
2020-12-15 23:07:47 -0300

    edit

--- maven.groovy
+++ maven.groovy
@@ -6,22 +6,22 @@
 def call() {
     stage('Compilar') {
         git( branch: 'main', url: 'https://github.com/criscubillos/ejemplo-maven.git')
-        sh 'mvn clean compile -e'
+        sh './mvnw clean compile -e'
     }
     stage('Test') {
-        sh 'mvn test -e'
+        sh './mvnw test -e'
     }
     stage('Empaquetar') {
-        sh 'mvn package -e -DskipTests=true'
+        sh './mvnw package -e -DskipTests=true'
     }
     stage('Ejecutar') {
-        sh 'mvn spring-boot:run &'
+        sh './mvnw spring-boot:run &'
     //sh 'sleep 10'
     //sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing' "
     }
     stage('Sonar') {
         withSonarQubeEnv(installationName: 'SonarQube Docker') {
-            sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
+            sh './mvnw org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
         }
     }
 }
