97762cc57fefaebb9a37bff4e22adf9df36d5356
Cristian <cristian.cubillos@editrade.cl>
2020-12-15 22:59:41 -0300

    ..

--- maven.groovy
+++ maven.groovy
@@ -5,51 +5,43 @@
 */
 
 def call() {
-  
-  pipeline {
-    agent any
-
-    tools {
-        maven "MVN 3.6.3"
-    }
-
-    stages {
-        stage('Compilar') {
-            steps {
-                git( branch: 'main',url: 'https://github.com/criscubillos/ejemplo-maven.git')
-                sh "mvn clean compile -e"
-            }
+    
+        agent any
+        tools {
+            maven 'MVN 3.6.3'
         }
-        stage('Test') {
-            steps {
-                sh "mvn test -e"
+        stages {
+            stage('Compilar') {
+                steps {
+                    git( branch: 'main', url: 'https://github.com/criscubillos/ejemplo-maven.git')
+                    sh 'mvn clean compile -e'
+                }
             }
-        }
-        stage('Empaquetar') {
-            steps {
-                sh "mvn package -e -DskipTests=true"
+            stage('Test') {
+                steps {
+                    sh 'mvn test -e'
+                }
             }
-        }
-        stage('Ejecutar'){
-            steps {
-                sh "mvn spring-boot:run &"
+            stage('Empaquetar') {
+                steps {
+                    sh 'mvn package -e -DskipTests=true'
+                }
+            }
+            stage('Ejecutar') {
+                steps {
+                    sh 'mvn spring-boot:run &'
                 //sh 'sleep 10'
                 //sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing' "
+                }
+            }
+            stage('Sonar') {
+                steps {
+                    withSonarQubeEnv(installationName: 'SonarQube Docker') {
+                        sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
+                    }
+                }
             }
         }
-
-        stage('Sonar'){
-        steps{
-            withSonarQubeEnv(installationName: 'SonarQube Docker') {
-                sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
-            }
-        }
-        }
-
-    }
+    
 }
-
-
-}
-
 return this
